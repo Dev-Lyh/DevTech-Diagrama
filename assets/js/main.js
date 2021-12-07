8829472c6a5efe7673d26d92d4aeb6d4f7d5a29a
EmilyFelicio <emilyfelicio03@gmail.com>
2021-12-07 18:25:36 -0300

    mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
 const bannerText = document.querySelector('.bannerText')
+
 bannerText.innerHTML = `Aprenda e se desenvolva através da DEV`
+
+const menuOpen = document.getElementById('openMenu')
+const menuClose = document.getElementById('closeMenu')
+const navBar = document.querySelector('.navBar')
+
+menuOpen.addEventListener('click', () => {
+  navBar.classList.add('active')
+  menuOpen.style.display = 'none'
+  menuClose.style.display = 'block'
+})
+
+menuClose.addEventListener('click', () => {
+  navBar.classList.remove('active')
+  menuOpen.style.display = 'block'
+  menuClose.style.display = 'none'
+})
